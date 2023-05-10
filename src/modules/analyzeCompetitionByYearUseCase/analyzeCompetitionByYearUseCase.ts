2a9c9121a5b11029d120e78df2e75e4049ca7cb2
Giovanni Janial <giovannijanial@gmail.com>
2023-05-10 00:17:56 -0300

    feat: new analyze

--- src/modules/analyzeCompetitionByYearUseCase/analyzeCompetitionByYearUseCase.ts
+++ src/modules/analyzeCompetitionByYearUseCase/analyzeCompetitionByYearUseCase.ts
@@ -1,7 +1,7 @@
 import { JsonToolkit, createJsonAgent } from "langchain/agents";
 import { OpenAI } from "langchain/llms/openai";
 
-import { JsonSpec } from "langchain/tools";
+import { JsonSpec, JsonListKeysTool } from "langchain/tools";
 import { competitionStatsRequester } from "../../utils";
 
 class AnalyzeCompetitionByYearUseCase {
@@ -16,31 +16,32 @@ class AnalyzeCompetitionByYearUseCase {
     }
 
 		const model = new OpenAI({ temperature: 0, modelName: "gpt-3.5-turbo" });
-    const input = `Get to know the relevant stats of a team and extract some static data from that team`;
+    const input = `Know some relevant statistics of a football league and extract some of them from the data of the teams in that league`;
 
+		try {
+			console.log('analyze starting...');
+			
+			const toolkit = new JsonToolkit(
+				new JsonSpec(competitionStatsRequesterResponse.data[0].TeamSeasons)
+			);
 
-    const filteredData = competitionStatsRequesterResponse.data.filter(
-			(season: any) => season.TeamSeasons[0].TeamId === 509
-    );
-    
-    console.log(filteredData[0].TeamSeasons[0]);
-    
- 
-		const toolkit = new JsonToolkit(
-			new JsonSpec(filteredData[0].TeamSeasons[0])
-		);
-		const executor = createJsonAgent(model, toolkit);
-		const result = await executor.call({ input });
+			const executor = createJsonAgent(model, toolkit);
+			const result = await executor.call({ input });
 
-		console.log(
-			`Got intermediate steps ${JSON.stringify(
-				result.intermediateSteps,
-				null,
-				2
-			)}`
-		);
+			console.log(
+				`Got intermediate steps ${JSON.stringify(
+					result.intermediateSteps,
+					null,
+					2
+				)}`
+			);
 
-		return result.output;
+			return { status: 200, data: result.output }
+		} catch (error) {
+			console.log("error: ", error);
+			return { status: 400, data: "error" };
+		}
+		
 	}
 }
 
